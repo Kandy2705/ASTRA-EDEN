7bc6f0da60b8ce8fa04da1ca020ddad2a70fd918
Ngô Ngọc Triệu Mẫn <147357803+Kandy2705@users.noreply.github.com>
2026-07-27 19:26:09 +0700

    fix: :art: chỉnh sửa bố cục UI

--- Assets/Textures/UI/Gentleland/SteampunkUI/Utils/Editor/OpenWindowsOnLoad.cs
+++ Assets/Textures/UI/Gentleland/SteampunkUI/Utils/Editor/OpenWindowsOnLoad.cs
@@ -1,9 +1,7 @@
 ﻿using UnityEditor;
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEditor.Compilation;
 
-// if you want to delete this file delete all Gentleland "Utils" folder 
+// if you want to delete this file delete all Gentleland "Utils" folder
 // you can then delete GentlelandSettings folder too
 namespace Gentleland.Utils.SteampunkUI
 {
